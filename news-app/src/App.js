16c2be31cc42f9fa295e541bb18980e07cdd627c
SiddarthaDarisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2023-02-26 12:35:12 -0500

    initial commit (working) with dependencies

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -1,26 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
-// importing components from react-router-dom package
-import {BrowserRouter as Router,Routes ,Route,Redirect,
-} from "react-router-dom";
-
-// import Home component
-import Home from "./components/Home";
 function App() {
-return (
-	<>
-	{/* This is the alias of BrowserRouter i.e. Router */}
-	<Router>
-		<Switch>
-		{/* This route is for home component
-		with exact path "/", in component props
-		we passes the imported component*/}
-		<Route path="/" element={<Home></Home>} />
-			
-		<Redirect to="/" />
-		</Switch>
-	</Router>
-	</>
-);
+  return (
+    <div className="App">
+     
+    </div>
+  );
 }
 
 export default App;
